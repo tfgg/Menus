625a3e43fdb40d9a6e44cd83c22645ecea640d79
Tim Green <timothy.green@gmail.com>
2011-11-15 23:15:54 +0000

    IMAP loaing of menus

--- proc.py
+++ proc.py
@@ -15,18 +15,6 @@ breakfast_type = MealType.objects.get(name='Breakfast')
 lunch_type = MealType.objects.get(name='Lunch')
 dinner_type = MealType.objects.get(name='Dinner')
 
-data = open(sys.argv[1]).read()
-
-data = quopri.decodestring(data)
-
-tree = html.document_fromstring(data)
-
-table = tree.xpath('//table')
-
-rows = table[0].xpath('//tr')
-
-start_date = datetime.date(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
-
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 def refine_coltext(col):
@@ -46,109 +34,127 @@ def refine_coltext(col):
 
   return (day, notes, items)
 
-week = None
-days = []
+def parse_html(data, start_date, dry_run=False):
+  tree = html.document_fromstring(data)
 
-for i, row in enumerate(rows):
-  num_cols = len(row.xpath('./td'))
+  table = tree.xpath('//table')
 
-  cols = row.xpath('./td')
+  rows = table[0].xpath('//tr')
 
-  brunch_day = False
 
-  if num_cols == 4:
-    day_name, notes, vegetarian = refine_coltext(cols[0])
-    _, _, breakfast = refine_coltext(cols[1])
-    _, _, lunch = refine_coltext(cols[2])
-    _, _, dinner = refine_coltext(cols[3])
-  elif num_cols == 3:
-    brunch_day = True
-    day_name, notes, vegetarian = refine_coltext(cols[0])
-    _, _, brunch = refine_coltext(cols[1])
-    _, _, dinner = refine_coltext(cols[2])    
+  week = None
+  days = []
 
-  day = weekdays[i-1]
+  for i, row in enumerate(rows):
+    num_cols = len(row.xpath('./td'))
 
-  date = start_date + datetime.timedelta(i-1)
+    cols = row.xpath('./td')
 
-  if i == 0:
-    week = day_name
-    continue
-  
-  if brunch_day:
-    print day, day_name
-    print "Brunch"
-    print " ", "\n  ".join(brunch)
-    print "Dinner"
-    print " ", "\n  ".join(dinner)
-    print "  Vegetarian:", vegetarian
+    brunch_day = False
+
+    if num_cols == 4:
+      day_name, notes, vegetarian = refine_coltext(cols[0])
+      _, _, breakfast = refine_coltext(cols[1])
+      _, _, lunch = refine_coltext(cols[2])
+      _, _, dinner = refine_coltext(cols[3])
+    elif num_cols == 3:
+      brunch_day = True
+      day_name, notes, vegetarian = refine_coltext(cols[0])
+      _, _, brunch = refine_coltext(cols[1])
+      _, _, dinner = refine_coltext(cols[2])    
+
+    day = weekdays[i-1]
+
+    date = start_date + datetime.timedelta(i-1)
+
+    if i == 0:
+      week = day_name
+      continue
+
+    Meal.objects.filter(date=date).delete()
     
-    brunch_obj = Meal.objects.create(type=brunch_type,
-                                     date=date,
-                                     notes="")
+    if brunch_day:
+      print day, day_name
+      print "Brunch"
+      print " ", "\n  ".join(brunch)
+      print "Dinner"
+      print " ", "\n  ".join(dinner)
+      print "  Vegetarian:", vegetarian
+      
+      if not dry_run:
+        brunch_obj = Meal.objects.create(type=brunch_type,
+                                         date=date,
+                                         notes="")
 
-    dinner_obj = Meal.objects.create(type=dinner_type,
-                                     date=date,
-                                     notes=" ".join(notes))
+        dinner_obj = Meal.objects.create(type=dinner_type,
+                                         date=date,
+                                         notes=" ".join(notes))
 
-    for i, item in enumerate(dinner):
-      MealItem.objects.create(meal=dinner_obj,
-                              name=item,
-                              order=i,
-                              vegetarian=False)
+        for i, item in enumerate(dinner):
+          MealItem.objects.create(meal=dinner_obj,
+                                  name=item,
+                                  order=i,
+                                  vegetarian=False)
 
-    for item in vegetarian:
-      i += 1
-      MealItem.objects.create(meal=dinner_obj,
-                              name=item,
-                              order=i,
-                              vegetarian=True)
-  
-  else:
-    print day, day_name
-    print "Breakfast"
-    print " ", "\n  ".join(breakfast)
-    print "Lunch"
-    print " ", "\n  ".join(lunch)
-    print "Dinner"
-    print " ", "\n  ".join(dinner)
-    print "  Vegetarian:", vegetarian
+        for item in vegetarian:
+          i += 1
+          MealItem.objects.create(meal=dinner_obj,
+                                  name=item,
+                                  order=i,
+                                  vegetarian=True)
+    
+    else:
+      print day, day_name
+      print "Breakfast"
+      print " ", "\n  ".join(breakfast)
+      print "Lunch"
+      print " ", "\n  ".join(lunch)
+      print "Dinner"
+      print " ", "\n  ".join(dinner)
+      print "  Vegetarian:", vegetarian
 
-    breakfast_obj = Meal.objects.create(type=breakfast_type,
+      if not dry_run:
+        breakfast_obj = Meal.objects.create(type=breakfast_type,
+                                            date=date,
+                                            notes="")
+        
+        lunch_obj = Meal.objects.create(type=lunch_type,
                                         date=date,
                                         notes="")
-    
-    lunch_obj = Meal.objects.create(type=lunch_type,
-                                    date=date,
-                                    notes="")
 
-    dinner_obj = Meal.objects.create(type=dinner_type,
-                                     date=date,
-                                     notes=" ".join(notes))
+        dinner_obj = Meal.objects.create(type=dinner_type,
+                                         date=date,
+                                         notes=" ".join(notes))
 
-    for i, item in enumerate(breakfast):
-      MealItem.objects.create(meal=breakfast_obj,
-                              name=item,
-                              order=i,
-                              vegetarian=False)
-    for i, item in enumerate(lunch):
-      MealItem.objects.create(meal=lunch_obj,
-                              name=item,
-                              order=i,
-                              vegetarian=False)
-    
-    for i, item in enumerate(dinner):
-      MealItem.objects.create(meal=dinner_obj,
-                              name=item,
-                              order=i,
-                              vegetarian=False)
+        for i, item in enumerate(breakfast):
+          MealItem.objects.create(meal=breakfast_obj,
+                                  name=item,
+                                  order=i,
+                                  vegetarian=False)
+        for i, item in enumerate(lunch):
+          MealItem.objects.create(meal=lunch_obj,
+                                  name=item,
+                                  order=i,
+                                  vegetarian=False)
+        
+        for i, item in enumerate(dinner):
+          MealItem.objects.create(meal=dinner_obj,
+                                  name=item,
+                                  order=i,
+                                  vegetarian=False)
 
-    for item in vegetarian:
-      i += 1
-      MealItem.objects.create(meal=dinner_obj,
-                              name=item,
-                              order=i,
-                              vegetarian=True)
+        for item in vegetarian:
+          i += 1
+          MealItem.objects.create(meal=dinner_obj,
+                                  name=item,
+                                  order=i,
+                                  vegetarian=True)
 
-#print week
-#print days
+  #print week
+  #print days
+if __name__ == "__main__":
+  data = open(sys.argv[1]).read()
+  data = quopri.decodestring(data)
+
+  start_date = datetime.date(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+  parse_html(data, date, False)
